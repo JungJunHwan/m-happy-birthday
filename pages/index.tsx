aa4fdd2d5369b80090e200fc43513f6da2c1aea7
JungJunHwan <qoqkd0221@naver.com>
2025-03-11 14:51:12 +0900

    mod

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,9 +9,9 @@ const Party = dynamic(() => import('../components/Firework'), {
 
 export default function Home() {
   return (
-    <div className='w-full min-h-screen flex justify-center items-center'>
+    <div className='w-full min-h-screen flex justify-center items-center overflow-x-hidden px-4'>
       <Party />
-      <div className='fixed top-11 text-white flex justify-center items-center flex-col'>
+      <div className='fixed top-11 md:top-11 text-white flex justify-center items-center flex-col w-full max-w-md mx-auto'>
         <Card name='가여닝' />
         {/* <Gift /> */}
         <Cake />
